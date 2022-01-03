15cac7dce3f164c44d3c4329475136cb3adec047
Nicolas Clavel <nc.nclavel@gmail.com>
2022-01-03 14:03:47 +0100

    update requirements for LINUX for Github actions

--- config/path_config.py
+++ config/path_config.py
@@ -5,8 +5,7 @@ PROJECT_DIRNAME = os.path.dirname(os.path.dirname(__file__))
 CONFIG_PATH = os.path.join(PROJECT_DIRNAME, 'config.json')
 PMC_PATH = os.path.join(os.path.dirname(PROJECT_DIRNAME), 'oa_file_list.txt')
 DATA_PATH = os.path.join(PROJECT_DIRNAME, 'data')
-
-LOG_PATH = os.path.join(PROJECT_DIRNAME, 'fixtures.log')
+LOG_PATH = os.path.join(PROJECT_DIRNAME, os.path.join('logs', 'logger.log'))
 
 # Urls
 OVH_AUTH_URL = 'https://auth.cloud.ovh.net/v3'
