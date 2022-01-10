6b1b1742d12b08977cfbf7fb5cf89f2f3f0bd8cb
Anne L'Hôte <anne.lhote@gmail.com>
2022-01-10 18:25:24 +0100

    [clean] Clean unused imports

--- harvest_pipeline.py
+++ harvest_pipeline.py
@@ -1,13 +1,9 @@
 import json
-import os
 
 from config.path_config import CONFIG_PATH
 from harvester.OAHarvester import OAHarvester
-from load_metadata import load_metadata
 
 if __name__ == '__main__':
-    # archive_path = load_metadata()
-
     config_harvester = json.load(open(CONFIG_PATH, 'r'))
     harvester = OAHarvester(config_harvester, thumbnail=False, sample=5, sample_seed=1)
 
