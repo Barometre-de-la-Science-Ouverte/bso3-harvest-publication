d4193633fe66630760d7a00cadd60ef0e1b9c5bf
Nicolas Clavel <nc.nclavel@gmail.com>
2022-01-17 15:46:38 +0100

    fixed tests unit

--- config/harvester_config.py
+++ config/harvester_config.py
@@ -1,5 +1,9 @@
 import json
-from config.path_config import CONFIG_PATH
+from config.path_config import CONFIG_PATH, CONFIG_PATH_TEST
 
-config_harvester = json.load(open(CONFIG_PATH, 'r'))
+try:
+    config_harvester = json.load(open(CONFIG_PATH, 'r'))
+except FileNotFoundError:
+    config_harvester = json.load(open(CONFIG_PATH_TEST, 'r'))
+
 NB_SAMPLE_TO_HARVEST = config_harvester['nb_samples_to_harvest']
--- config/path_config.py
+++ config/path_config.py
@@ -3,6 +3,7 @@ import os
 # Paths
 PROJECT_DIRNAME = os.path.dirname(os.path.dirname(__file__))
 CONFIG_PATH = os.path.join(PROJECT_DIRNAME, 'config.json')
+CONFIG_PATH_TEST = os.path.join(PROJECT_DIRNAME, 'tests', 'unit_tests', 'config_test.json')
 DATA_PATH = os.path.join(PROJECT_DIRNAME, 'data')
 LOG_PATH = os.path.join(PROJECT_DIRNAME, os.path.join('logs', 'logger.log'))
 LOCAL_METADATA_PATH = 'tmp/bso-publications-staging_20211119_sample_5k.jsonl.gz'
