621fb69adb59de9a8734f401d97184217d0b38be
ANBR <abres@octo.com>
2022-01-31 16:35:50 +0100

    Change swift download_file to download_files and fix calls to swift methods: missing container parameter

--- ovh_handler.py
+++ ovh_handler.py
@@ -15,7 +15,7 @@ def generateStoragePath(identifier):
 
 def get_partitions(_swift: Swift, partition_size: int) -> List:
     """Return a list a partitions of gzipped or flat pdf files"""
-    files = _swift.get_swift_list()
+    files = _swift.get_swift_list(_swift.config['publications_dump'])
     files = [file for file in files if (file.endswith('.pdf.gz') or file.endswith('.pdf'))]
     partitions = [files[i:i + partition_size] for i in range(0, len(files), partition_size)]
     return partitions
@@ -24,7 +24,7 @@ def get_partitions(_swift: Swift, partition_size: int) -> List:
 def download_files(_swift, dest_dir, files):
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir)
-    return _swift.download_files(files, dest_dir)
+    return _swift.download_files(_swift.config['publications_dump'], files, dest_dir)
 
 
 def upload_and_clean_up(_swift, local_dir):
@@ -32,7 +32,7 @@ def upload_and_clean_up(_swift, local_dir):
     upload_files = [
         (file, os.path.join('processed', generateStoragePath(os.path.basename(file)), os.path.basename(file)))
         for file in files if not (file.endswith('.pdf') or file.endswith('.pdf.gz'))]
-    _swift.upload_files_to_swift(upload_files)
+    _swift.upload_files_to_swift(_swift.config['publications_dump'], upload_files)
     for file in files:
         os.remove(file)
 
