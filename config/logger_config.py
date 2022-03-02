712333c70f79bbd4f86be1e557f253d2dd6845a8
Nicolas Clavel <nc.nclavel@gmail.com>
2022-03-02 16:38:57 +0100

    fix config logger

--- config/logger_config.py
+++ config/logger_config.py
@@ -2,7 +2,7 @@ import logging
 
 from config.harvester_config import config_harvester
 
-is_debug_level = config_harvester['is_debug_level']
+is_debug_level = config_harvester['is_level_debug']
 if is_debug_level == 1:
     LOGGER_LEVEL = logging.DEBUG
 else:
