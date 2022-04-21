bd99b827c22d23a437702bf9d75070747df7d8cc
niel-octo <95078019+niel-octo@users.noreply.github.com>
2022-04-21 11:09:27 +0200

    Update backup_postgres.sh

--- backup_postgres.sh
+++ backup_postgres.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-## Argument MOUNTED_ENDPOINT_POSTGRES_BACKUP has to be supplied, else the standard value /var/lib/postgresql/data is put
-## MOUNTED_ENDPOINT_POSTGRES_BACKUP = /var/lib/postgresql/data
-## The idea would be to launch a CRON every week to backup the DB, but it needs to remove the previous backup... Which is not
-## done yet in this script.
 
-## LAUNCH this script by running : bash backup_postgres.sh [path_to_save_db_dump]
-## LAUNCH in the postgres pod
+##-------------------------------------------------------------------------------------------------------------------
+# Argument: MOUNTED_ENDPOINT_POSTGRES_BACKUP should be supplied, otherwise it will be /var/lib/postgresql/data is put
+# Recommendation: launch a CRON every week to backup the DB.
+# Careful: old backups are removed before dumping current backup
+# Launch: enter in the postgres pods (kubectl exec -it [POD_ID] -- bash) and run ./df_migration/bash backup_postgres.sh [PATH_TO_DB_DUMP]
+# To recover: run : CREATE DATABASE targetdb; psql -U postgres -d targetdb -f [PATH_TO_DB_DUMP]
+##-------------------------------------------------------------------------------------------------------------------
 
-## Check if arguments supplied
+##----- 1. File location
+#  Check if arguments supplied and put it in MOUNTED_ENDPOINT_POSTGRES_BACKUP
 if [ -z "$1" ] || [ $# -eq 0 ]
   then
     echo "No argument supplied"
@@ -16,15 +18,14 @@ if [ -z "$1" ] || [ $# -eq 0 ]
     MOUNTED_ENDPOINT_POSTGRES_BACKUP="$1"
 fi
 
-echo $MOUNTED_ENDPOINT_POSTGRES_BACKUP
-
 TODAY="$(date +'%F')"
 DB_DUMP_NAME=db_dump_$TODAY.sql
-echo $DB_DUMP_NAME
-echo $MOUNTED_ENDPOINT_POSTGRES_BACKUP/$DB_DUMP_NAME
 
+##----- 2. remove old one
+find $MOUNTED_ENDPOINT_POSTGRES_BACKUP -name '*db_dump*' -delete
+echo Remove old db dumps in $MOUNTED_ENDPOINT_POSTGRES_BACKUP...
+
+##----- 3. Dump backup
+echo Dump backup of postgres_db here : $MOUNTED_ENDPOINT_POSTGRES_BACKUP/$DB_DUMP_NAME ...
 pg_dump -U postgres -d postgres_db -f $MOUNTED_ENDPOINT_POSTGRES_BACKUP/$DB_DUMP_NAME
-
-# To read the db dump :
-# CREATE DATABASE targetdb;
-# psql -U postgres -d targetdb -f $DB_DUMP_NAME
+echo Successfully dumped backup of postgres_db here : $MOUNTED_ENDPOINT_POSTGRES_BACKUP/$DB_DUMP_NAME
