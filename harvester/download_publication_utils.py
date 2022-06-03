bd260c982fd226866c95abaaedf1202d51ea934b
ANBR <abres@octo.com>
2022-06-03 18:59:47 +0200

    Fix issue with arxiv files not getting updated in DB and Refacto

--- harvester/download_publication_utils.py
+++ harvester/download_publication_utils.py
@@ -1,3 +1,4 @@
+import os
 import re
 import subprocess
 from time import sleep
@@ -10,12 +11,21 @@ from application.server.main.logger import get_logger
 from config.logger_config import LOGGER_LEVEL
 from harvester.exception import EmptyFileContentException, PublicationDownloadFileException
 from utils.file import is_file_not_empty
+from harvester.gzip_utils import decompress
 
 logger = get_logger(__name__, level=LOGGER_LEVEL)
 
+SUCCESS_DOWNLOAD = 'success'
+FAIL_DOWNLOAD = 'fail'
+ARXIV_HARVESTER = 'arxiv'
+STANDARD_HARVESTER = 'standard'
+WILEY_HARVESTER = 'wiley'
+EMPTY_URL = ''
+EXCEPTION_HARVESTER = ''
+
 
 def _download_publication(urls, filename, local_entry, wiley_client):
-    result = 'fail'
+    result = FAIL_DOWNLOAD
     doi = local_entry['doi']
     logger.info(f'*** Start downloading the publication with doi = {doi}. {len(urls)} urls will be tested.')
     for url in urls:
@@ -23,11 +33,11 @@ def _download_publication(urls, filename, local_entry, wiley_client):
             logger.debug(f"Doi = {doi}, Publication URL to download = {url}")
             if 'arxiv' in url:
                 result, harvester_used = arxiv_download(url, filename, doi)
-                if result == 'success':
+                if result == SUCCESS_DOWNLOAD:
                     break
             elif 'wiley' in url:
                 result, harvester_used = wiley_download(doi, filename, wiley_client)
-                if result == 'success':
+                if result == SUCCESS_DOWNLOAD:
                     break
             # standard download always done if other methods do not work
             result, harvester_used = standard_download(url, filename, doi)
@@ -43,11 +53,14 @@ def _download_publication(urls, filename, local_entry, wiley_client):
 
 def arxiv_download(url: str, filepath: str, doi: str) -> (str, str):
     from config.swift_cli_config import init_cmd
-    file_path = url_to_path(url)
-    subprocess.check_call(f'{init_cmd} download arxiv_harvesting {file_path} -o {filepath}', shell=True)
-    result, harvester_used = 'fail', 'arxiv'
-    if is_file_not_empty(filepath):
-        result = 'success'
+    ovh_arxiv_file_pdf_gz = url_to_path(url)
+    filepath_gz = filepath + ".gz"
+    subprocess.check_call(f'{init_cmd} download arxiv_harvesting {ovh_arxiv_file_pdf_gz} -o {filepath_gz}', shell=True)
+    result, harvester_used = FAIL_DOWNLOAD, ARXIV_HARVESTER
+    if is_file_not_empty(filepath_gz):
+        result = SUCCESS_DOWNLOAD
+        decompress(filepath_gz)
+        os.remove(filepath_gz)
         logger.debug(f'The publication with doi = {doi} was successfully downloaded via arXiv_harvesting. url = {url}')
     else:
         logger.warning(f'The publication with doi = {doi} download failed via arXiv_harvesting. url = {url}')
@@ -70,7 +83,7 @@ def standard_download(url: str, filename: str, doi: str) -> (str, str):
     logger.debug(f'The publication with doi = {doi} was successfully downloaded via standard request')
     with open(filename, 'wb') as f_out:
         f_out.write(content)
-    result, harvester_used = 'success', 'standard'
+    result, harvester_used = SUCCESS_DOWNLOAD, STANDARD_HARVESTER
     return result, harvester_used
 
 
